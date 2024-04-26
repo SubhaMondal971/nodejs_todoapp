0bcc85b54602641d9582d1e670e879873796a579
Subha Mondale <subhamondal971gmail.com>
2024-04-26 21:01:46 +0530

    initial commit

--- data/database.js
+++ data/database.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export const connectDB = ()=>{
   mongoose.connect(process.env.MONGO_URI,{
   dbName:"backendapi"
-}).then(()=>{
-  console.log("connected")
+}).then((c)=>{
+  console.log(`connected with ${c.connection.host}`)
 }).catch((err)=>console.log(err));
 }
